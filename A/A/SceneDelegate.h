76648295d0c8e6666094fbd488e8d921edcdaa37
hang-zhang885 <zhanghang@192.168.0.102>
2024-04-01 18:20:25 +0800

    新建工程A

---
+++ A/A/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  A
+//
+//  Created by 张航 on 2024/4/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ A/A/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  A
+//
+//  Created by 张航 on 2024/4/1.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
